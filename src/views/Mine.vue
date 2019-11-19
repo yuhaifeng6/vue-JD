ccf232d7e6d0963f25268c24afda9a0b2a1f7c39
于海峰 <13256175586@163.com>
2019-11-19 22:09:31 +0800

    我的页面

--- src/views/Mine.vue
+++ src/views/Mine.vue
@@ -1,6 +1,11 @@
 <template>
   <div class='mine'>
-      我的
+      <ul>
+        <li v-for="item, index in arr" class="mine_li" :key="index" @click="change(item)">
+          <span class="mine_span">{{item.label}}</span>
+          <i class="cubeic-arrow"></i>
+        </li>
+      </ul>
   </div>
 </template>
 
@@ -9,6 +14,13 @@ export default {
 name: 'mine',
   data(){
     return {
+      arr: [
+        { label: "商品收藏" },
+        { label: "我的足迹" },
+        { label: "店铺收藏" },
+        { label: "我的订单" },
+        { label: "退出", type: "exit" },
+      ]
     };
   },
 
@@ -16,9 +28,29 @@ name: 'mine',
 
   mounted() {},
 
-  methods: {},
+  methods: {
+    change(val){
+      if (val.type == "exit") {
+        this.$store.commit("setToken", "")
+        localStorage.removeItem("token")
+        this.$router.push("/login")
+      }
+    }
+  },
 }
 
 </script>
 <style lang='scss' scoped>
+.mine_li{
+  font-size: 14px;
+  text-align: left;
+  height: 50px;
+  line-height: 50px;
+  padding-left: 5%;
+  border-bottom: 1px solid #eee;
+  .mine_span{
+    display: inline-block;
+    width: 90%;
+  }
+}
 </style>
